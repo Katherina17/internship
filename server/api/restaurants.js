710983f8916d8232ea6eb0e7e408c90c0919470b
katherina17 <katherinepetrova76@gmail.com>
2022-03-04 19:10:36 +0300

    feat: added a server functionality

--- server/api/restaurants.js
+++ server/api/restaurants.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const Restaurants = require('../models/Restaurants');
+const Restaurants = require('../models/Restaurants')
 
 router.get('/', async (req, res) => {
-    try {
-        const films = await Restaurants.find();
-        res.json(films)
-    } catch(err){
-        res.json({message: err})
-    }
+    const pageNumber = req.query.pageNumber;
+    const pageSize = req.query.pageSize;
+    const query  = {
+        skip: pageSize * (pageNumber - 1),
+        limit: pageSize
+    };
+    Restaurants.find({}, {}, query, function(err, data) {
+        if (err) {
+            response = {'status': 200, 'message': 'Error fetching data'};
+        } else {
+            response = {'data': data};
+        }
+        res.json(response);
+    });
 });
 
 module.exports = router
